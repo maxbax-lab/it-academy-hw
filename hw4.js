3babb45e97bdd1945ec5bee3acef07b2f32be29b
maxbax-lab <58370967+maxbax-lab@users.noreply.github.com>
2020-01-29 16:33:33 +0300

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,14 @@
+const arr = [1,2,3,[4,5]];
+
+function arrSum (arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++ ){
+        if(Array.isArray(arr[i])){
+            sum += arrSum(arr[i])
+        } else {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+console.log(arrSum(arr));
