65d10fdcf5fe8277b7151b42724183265b3ff997
maxbax-lab <58370967+maxbax-lab@users.noreply.github.com>
2020-02-01 01:22:02 +0300

    Update hw5.2.js

--- hw5.2.js
+++ hw5.2.js
@@ -12,15 +12,13 @@ const objTwo = {
 const objOneKeyArr = (Object.keys(objOne));
 
 function deepEqual(objOne, objTwo){
-    const objOneKeysLength = (Object.keys(objOne)).length;
-    const objTwoKeysLength = (Object.keys(objTwo)).length;
     const objOneKeyArr = Object.keys(objOne);
     const objTwoKeyArr = Object.keys(objTwo);
     const objOneValueArr = Object.values(objOne);
     const objTwoValueArr = Object.values(objTwo);
-    if (objOneKeysLength != objTwoKeysLength){
+    if (objOneKeyArr.length != objTwoKeyArr.length){
         return "false";
-    } else if (objOneKeysLength === objTwoKeysLength) {
+    } else if (objOneKeyArr.length = objTwoKeyArr.length) {
         for (let i = 0; i < objOneKeyArr.length; i++){          
             for (let j = 0; j < objTwoKeyArr.length; j++){               
                 if (objOneKeyArr[i] === objTwoKeyArr[j] && objOneValueArr[i] === objTwoValueArr[j]){
